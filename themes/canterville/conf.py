472cf4f881e24f535bfc74a3ab0d970517d0ba3b
Susan Tan <susan@getwellio.com>
2020-10-04 11:45:21 -0700

    First website

---
+++ themes/canterville/conf.py
@@ -0,0 +1,9 @@
+THEME = 'canterville'
+LOGO_URL = 'images/logo.png'
+BANNER_URL = '/assets/img/silk-road.jpg'
+GLOBAL_CONTEXT = {
+        'TWITTER_URL': 'https://twitter.com/getnikola',
+        'GITHUB_URL': 'https://github.com/getnikola',
+        'LINKEDIN_URL': None,
+        'BANNER_URL': '/assets/img/silk-road.jpg'
+}
